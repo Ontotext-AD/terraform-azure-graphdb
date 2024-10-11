2573c5efb56159fc6090c0ce3394c0b8f9fb1af8
kristianiliev1 <148194420+kristianiliev1@users.noreply.github.com>
2024-10-11 16:20:11 +0300

    Update Description for tls_certificate_path -  variables.tf

--- variables.tf
+++ variables.tf
@@ -174,7 +174,7 @@ variable "gateway_probe_threshold" {
 
 # TLS
 variable "tls_certificate_path" {
-  description = "Path to a TLS certificate that will be imported in Azure Key Vault and used in the Application Gateway TLS listener for GraphDB. Either tls_certificate_path or tls_certificate_password must be provided."
+  description = "Path to a TLS certificate that will be imported in Azure Key Vault and used in the Application Gateway TLS listener for GraphDB. Either tls_certificate_path or tls_certificate_id must be provided."
   type        = string
 }
 
