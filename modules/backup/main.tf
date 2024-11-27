2127af65b240ed19cd7e51167078e52a2c6ac1a2
templier2 <61189166+templier2@users.noreply.github.com>
2024-11-27 15:59:25 +0100

    Update main.tf
    Remove enable_https_traffic_only because it is default too (also it was changed in newer versions of AzureRM).

--- modules/backup/main.tf
+++ modules/backup/main.tf
@@ -28,7 +28,6 @@ resource "azurerm_storage_account" "graphdb_backup" {
   account_kind                      = var.storage_account_kind
   account_tier                      = var.storage_account_tier
   account_replication_type          = var.storage_account_replication_type
-  enable_https_traffic_only         = true
   allow_nested_items_to_be_public   = false
   infrastructure_encryption_enabled = true
   allowed_copy_scope                = var.storage_account_allowed_copy_scope
