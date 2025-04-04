48ba65bb953a9b909c18aa5a5d6e5c0e12d9add9
Mihail Radkov <mihail.radkov@ontotext.com>
2025-04-04 11:41:34 +0300

    Update GraphDB to version 10.8.5
    Updated the default GraphDB version to the latest patch version.

--- variables.tf
+++ variables.tf
@@ -246,7 +246,7 @@ variable "admin_security_principle_id" {
 variable "graphdb_version" {
   description = "GraphDB version from the marketplace offer"
   type        = string
-  default     = "10.8.4"
+  default     = "10.8.5"
 }
 
 variable "graphdb_sku" {
