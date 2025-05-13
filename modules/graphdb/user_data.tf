6893ebf0ab1ad28154827b30a27f9692d95bbf34
Ivan Konstantinov <ivan.konstantinov@ontotext.com>
2025-05-13 15:33:35 +0300

    Set graphdb to be sudoer (for testing)

--- modules/graphdb/user_data.tf
+++ modules/graphdb/user_data.tf
@@ -178,12 +178,4 @@ data "cloudinit_config" "entrypoint" {
       content      = templatefile(part.value["path"], part.value["variables"])
     }
   }
-
-  part {
-    content_type = "text/x-shellscript"
-    content      = <<-EOF
-        #!/bin/bash
-        [[ -f /etc/sudoers.d/90-cloud-init-users ]] && rm /etc/sudoers.d/90-cloud-init-users
-      EOF
-  }
 }
